dbd52452cf1ef4dbf26fb6fa502aa49fcabf8156
Pranav Kumar <pk2051872@gmail.com>
2026-08-20 15:34:08 +0530

    LC POTD - Distribute Elements Into Two Arrays I
    Implement solution for LeetCode problem 3069, distributing elements into two arrays based on simulation.

---
+++ 19_Aug_2026.py
@@ -0,0 +1,20 @@
+# LeetCode POTD - 2026-08-19
+# 3069. Distribute Elements Into Two Arrays I
+# Approach: Simulation
+
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        arr1 = []
+        arr2 = []
+        
+        arr1.append(nums[0])
+        arr2.append(nums[1])
+        
+        for i in range(2, n):
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
+                
+        return arr1 + arr2
